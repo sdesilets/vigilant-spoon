bac875fc82411d021093eab95d6ce4aefb669341
Sebastien Desilets <solantd@gmail.com>
2018-04-15 21:51:08 -0400

    Guard Part 2

--- Source/FPSGame/Private/FPSAIGuard.cpp
+++ Source/FPSGame/Private/FPSAIGuard.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FPSAIGuard.h"
+#include "Perception/PawnSensingComponent.h"
+#include "Components/PawnNoiseEmitterComponent.h"
+#include "DrawDebugHelpers.h"
 
 
 // Sets default values
@@ -9,6 +12,23 @@ AFPSAIGuard::AFPSAIGuard()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComp"));
+
+	PawnSensingComp->OnSeePawn.AddDynamic(this, &AFPSAIGuard::HandleSeePawn);
+	PawnSensingComp->OnHearNoise.AddDynamic(this, &AFPSAIGuard::HandleHearNoise);
+}
+
+void AFPSAIGuard::HandleSeePawn(APawn* SeenPawn)
+{
+	if (SeenPawn)
+	{
+		DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Red, false, 10.0f);
+	}
+}
+
+void AFPSAIGuard::HandleHearNoise(APawn* NoiseInstigator, const FVector& Location, float Volume)
+{
+	DrawDebugSphere(GetWorld(), NoiseInstigator->GetActorLocation(), 32.0f, 12, FColor::Green, false, 10.0f);
 }
 
 // Called when the game starts or when spawned
@@ -24,11 +44,3 @@ void AFPSAIGuard::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
-// Called to bind functionality to input
-void AFPSAIGuard::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
--- Source/FPSGame/Public/FPSAIGuard.h
+++ Source/FPSGame/Public/FPSAIGuard.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "FPSAIGuard.generated.h"
 
+class UPawnSensingComponent;
+
 UCLASS()
 class FPSGAME_API AFPSAIGuard : public ACharacter
 {
@@ -19,13 +21,14 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UPawnSensingComponent* PawnSensingComp;
+
+	UFUNCTION()
+		void HandleSeePawn(APawn* Pawn);
+	UFUNCTION()
+		void HandleHearNoise(APawn* NoiseInstigator, const FVector& Location, float Volume);
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	
-	
 };
